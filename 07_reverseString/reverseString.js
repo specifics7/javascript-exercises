d0574e20c970d33571b95876558bef0fcd1a26ba
specifics7 <specifix456@gmail.com>
2025-08-22 23:51:35 -0700

    Solve exercise 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let arrStr = str.split('').reverse().join('');
 
+    return arrStr;
 };
+
+console.log(reverseString("Hello There!"));
 
 // Do not edit below this line
 module.exports = reverseString;
