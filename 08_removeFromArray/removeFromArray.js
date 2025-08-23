5fb510b76ce1b6513dbc53a95052032ab3dc8493
specifics7 <specifix456@gmail.com>
2025-08-23 00:39:16 -0700

    Solve exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...toRemove) {
+    return arr.filter(val => !toRemove.includes(val));
+}
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
