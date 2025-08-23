66c64268237b2e1509761bdc8e7225ba2e29ed5e
specifics7 <specifix456@gmail.com>
2025-08-22 23:36:25 -0700

    Solve exercise 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, totalCount) {
 
+    let repeatedString = '';
+    // Return 'ERROR' if totalCount is negative 
+    if (totalCount < 0) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < totalCount; i++) {
+        repeatedString = repeatedString + str;
+    }
+
+    return repeatedString;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
